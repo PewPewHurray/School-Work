8da325840957d11e2fe34a55ef805e88105556b8
Chris Sisk <dogsrule_2003@yahoo.com>
2022-01-31 19:46:24 -0800

    Removed useless style and changed html reference

--- Python/flask/fundamentals/Chris_Sisk_Playground/server.py
+++ Python/flask/fundamentals/Chris_Sisk_Playground/server.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 @app.route("/play")
 def three_squares():
 
-    return render_template("checkerboard.html", times = 3)
+    return render_template("playground.html", times = 3)
 
 @app.route("/play/<int:num>")
 def times_squares(num):
     
-    return render_template("checkerboard.html", times = num)
+    return render_template("playground.html", times = num)
 
 @app.route("/play/<int:num>/<string:color>")
 def times_squares_color(num, color):
     
-    return render_template("checkerboard.html", times = num, square_color = color)
+    return render_template("playground.html", times = num, square_color = color)
 
 if __name__=="__main__":
     app.run(debug=True)
