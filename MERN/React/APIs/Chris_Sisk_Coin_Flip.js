345c39e775b9803794902f982c8f64e28400b1c4
Chris Sisk <dogsrule_2003@yahoo.com>
2022-03-24 17:13:23 -0700

    adjusted if else resolve reject

--- MERN/React/APIs/Chris_Sisk_Coin_Flip.js
+++ MERN/React/APIs/Chris_Sisk_Coin_Flip.js
@@ -4,19 +4,21 @@ const tossCoin = () => {
 const fiveHeads = new Promise((resolve, reject) => {
     let heads = 0;
     let attempts = 0;
+    let maxAttempts = 100;
     while (attempts <= 100 && heads < 5){
         attempts++;
         const result = tossCoin();
+        console.log(result)
         if(result === "heads"){
             heads++;
         } else {
             heads = 0;
         }
-        if (heads === 5){
-            resolve(`It took ${attempts} attempts to get 5 heads in a row.`);
-        } else if (attempts === 100) {
-            reject("After 100 attempts did not get 5 heads in a row.");
-        }
+    }
+    if (attempts <= maxAttempts){
+        resolve(`It took ${attempts} attempts to get 5 heads in a row.`);
+    } else {
+        reject("After 100 attempts did not get 5 heads in a row.");
     }
 });
 
